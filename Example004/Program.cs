f136df9bc648995890070ec1744e919df4dbdea4
Amirova Milyausha <mila17.86@mail.ru>
2022-08-26 00:47:04 +0300

    Четвертое задание

---
+++ Example004/Program.cs
@@ -0,0 +1,11 @@
+﻿int start = 100;
+int stop = 1000000;
+int value = new Random().Next(start,stop);
+if ((value%7==0) && (value%23==0))
+{
+    Console.WriteLine("Да, кратно");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
