f1f165494c261fe1d4048e8a4da5acc5b41d252e
Amirova Milyausha <mila17.86@mail.ru>
2022-08-26 00:14:19 +0300

    Третье задание

---
+++ Example003/Program.cs
@@ -0,0 +1,14 @@
+﻿int a;
+Console.WriteLine("Введите число от 1 до 7");
+a=int.Parse(Console.ReadLine());
+if (a<=5)
+{
+ Console.WriteLine ("Нет,рабочий");
+}
+else if (a<=7)
+{
+ Console.WriteLine ("Да,выходной");
+}
+else{
+    Console.WriteLine ("Введенное число не соответствует условиям");
+}
