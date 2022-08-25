7dc67159b6550565f3ad04e3ae34dbf43a7954d2
Amirova Milyausha <mila17.86@mail.ru>
2022-08-26 00:31:03 +0300

    Третье задание_1

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,14 +1,15 @@
 ﻿int a;
 Console.WriteLine("Введите число от 1 до 7");
 a=int.Parse(Console.ReadLine());
-if (a<=5)
-{
- Console.WriteLine ("Нет,рабочий");
-}
-else if (a<=7)
-{
- Console.WriteLine ("Да,выходной");
-}
-else{
-    Console.WriteLine ("Введенное число не соответствует условиям");
-}
+switch (a)
+ {
+    case <=5:
+    Console.WriteLine("Нет, рабочий");
+    break;
+     case <=7:
+    Console.WriteLine("Да, выходный");
+    break;
+    default:
+    Console.WriteLine("Введенное число не соответствует условиям!");
+    break;
+ }
