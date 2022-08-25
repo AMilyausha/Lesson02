6746533184e643caa526540be7efbe2e8f10ea51
Amirova Milyausha <mila17.86@mail.ru>
2022-08-25 23:34:21 +0300

    Первое задание

---
+++ Example001/Program.cs
@@ -0,0 +1,10 @@
+﻿int start = 100;
+int stop = 1000;
+int value = new Random().Next(start,stop);
+string a  = value.ToString();
+char[] num = new char[a.Length];
+for(int index = 0; index < a.Length; index++){
+    num[index] = a[index];
+    }
+Console.WriteLine(value);
+Console.WriteLine(num[1]);
