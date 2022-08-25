c0233c7c918404c3c9f2c0d2c2b7ce6a51e90790
Amirova Milyausha <mila17.86@mail.ru>
2022-08-25 23:58:31 +0300

    Второе задание

---
+++ Example002/Program.cs
@@ -0,0 +1,15 @@
+﻿int start = 1;
+int stop = 65537;
+int value = new Random().Next(start,stop);
+string a  = value.ToString();
+char[] num = new char[a.Length];
+for(int index = 0; index < a.Length; index++){
+    num[index] = a[index];
+    }
+if (value <=99){
+    Console.WriteLine("Третьей цифры нет");
+}
+else {
+Console.WriteLine(value);
+Console.WriteLine(num[2]);
+}
